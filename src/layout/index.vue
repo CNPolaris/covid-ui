6feaea2c3bf68616960824874b234a3fe2f755c3
CNPolaris <42991837+CNPolaris@users.noreply.github.com>
2022-05-04 13:47:44 +0800

    init

---
+++ src/layout/index.vue
@@ -0,0 +1,83 @@
+<template>
+  <el-container>
+    <el-main class="student-main">
+      <router-view />
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import ResizeMixin from './mixin/ResizeHandler'
+
+export default {
+  name: 'Layout',
+  mixins: [ResizeMixin],
+  data() {
+    return {
+      defaultUrl: '/',
+      userInfo: {
+        imagePath: null
+      }
+    }
+  },
+  watch: {
+    $route(to, from) {
+      this.defaultUrl = this.routeSelect(to.path)
+    }
+  },
+  methods: {
+    routeSelect(path) {
+      const topPath = ['/', '/index', '/paper/index', '/record/index', '/question/index']
+      if (topPath.indexOf(path)) {
+        return path
+      }
+      return null
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+  @import "~@/styles/mixin.scss";
+  @import "~@/styles/variables.scss";
+
+  .app-wrapper {
+    @include clearfix;
+    position: relative;
+    height: 100%;
+    width: 100%;
+
+    &.mobile.openSidebar {
+      position: fixed;
+      top: 0;
+    }
+  }
+
+  .drawer-bg {
+    background: #000;
+    opacity: 0.3;
+    width: 100%;
+    top: 0;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+  }
+
+  .fixed-header {
+    position: fixed;
+    top: 0;
+    right: 0;
+    z-index: 9;
+    width: calc(100% - #{$sideBarWidth});
+    transition: width 0.28s;
+  }
+
+  .hideSidebar .fixed-header {
+    width: calc(100% - 54px)
+  }
+
+  .mobile .fixed-header {
+    width: 100%;
+  }
+</style>
